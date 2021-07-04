ac1f1b3a45e4f1c270f521ea550fb67a237fa82e
davidyoon <qqpo12@naver.com>
2021-07-04 23:02:24 +0900

    add pickerView on addView

--- TableView/AddViewController.swift
+++ TableView/AddViewController.swift
@@ -7,19 +7,62 @@
 
 import UIKit
 
-class AddViewController: UIViewController {
+class AddViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet var tfAddItem: UITextField!
+    @IBOutlet var pickerView: UIPickerView!
+    
+    var imageArray = [UIImage?]()
+    var imageFileName = ["cart.png", "clock.png", "pencil.png"]
+    let MAX_ARRAY_NUM = 3
+    let PICKER_VIEW_COLUMN = 1
+    let PICKER_VIEW_HEIGHT: CGFloat = 40
+    
+    var fileName = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        for i in 0..<MAX_ARRAY_NUM {
+            let image = UIImage(named: imageFileName[i])
+            imageArray.append(image)
+        }
+        
+        
+        fileName = imageFileName[0]
+        
+        
     }
     
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return PICKER_VIEW_COLUMN
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return imageFileName.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let imageView = UIImageView(image: imageArray[row])
+        imageView.frame = CGRect(x:0,y:0, width: 40, height: 40)
+        
+        return imageView
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return imageFileName[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return PICKER_VIEW_HEIGHT
+    }
 
     @IBAction func btnAddItem(_ sender: Any) {
         items.append(tfAddItem.text!)
-        itemsImageFile.append("clock.png")
+        itemsImageFile.append(fileName)
         tfAddItem.text = ""
         _ = navigationController?.popViewController(animated: true)
     }
